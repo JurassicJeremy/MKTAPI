4d95e1a7d8ef868851fb0070a097be1f6d25e3cd
Jeremy Burnham <25983307+JurassicJeremy@users.noreply.github.com>
2021-05-03 22:06:46 -0500

    Updated syntax for order count when using COUNT API. Need to resolve lines 18/19 for correct count. Comment added.

--- getOrders.py
+++ getOrders.py
@@ -12,62 +12,65 @@ headers = {
 # GET API to obtain count of new orders
 response = requests.request("GET", countURL, headers=headers, data=payload)
 
-#need to allow for any numbers
-if ":1" in response.text:
-    #need to show how many new orders there are
-    print('\nYou have','new orders, would you like to download them?\n')
+count = json.loads(response.text)
 
-    answer = input("Type y for yes, n for no: ", )
+#resolve issue with incorrect data showing. Shows count even when there are no orders.
+for count['response'] in response.text:
+    if count['response'] >= '0':
 
-    if answer == "y":
-        detailsURL = "https://api.reptimeqa.com/reptime/public/api/orders/export/M32685/open"
+        print('\nYou have', count['response'], 'new order(s), would you like to download them?\n')
 
-        # GET API to obtain new order details
-        response = requests.request("GET", detailsURL, headers=headers, data=payload)
-        results = response.json()
+        answer = input("Type y for yes, n for no: ", )
 
-        while True:
-            print("*" * 25)
-            print("What would you like to do?")
-            print("*" * 25)
-            print("d = Download Data File")
-            print("v = View Raw Order Data")
-            print("u = Update Order Status")
-            print("q = Quit")
-            print("*" * 25)
-            answerMenu = input(":")
+        if answer == "y":
+            detailsURL = "https://api.reptimeqa.com/reptime/public/api/orders/export/M32685/open"
 
+            # GET API to obtain new order details
+            response = requests.request("GET", detailsURL, headers=headers, data=payload)
+            results = response.json()
 
-            if answerMenu == "d":
-                # Creates TXT file, and adds API response to file
-                with open("apiResults1.txt", "w") as file:
-                    file.write(json.dumps(results))
-                    print("File Opened")
-                    os.startfile("apiResults1.txt")
-
-            if answerMenu == "v":
-                print("Here is the raw data:\n", response.content)
+            while True:
+                print("*" * 25)
+                print("What would you like to do?")
+                print("*" * 25)
+                print("d = Download Data File")
+                print("v = View Raw Order Data")
+                print("u = Update Order Status")
+                print("q = Quit")
+                print("*" * 25)
+                answerMenu = input(":")
 
 
-            if answerMenu == "q":
-                print("\nThank you, see you soon!")
-                break
+                if answerMenu == "d":
+                    # Creates TXT file, and adds API response to file
+                    with open("apiResults1.txt", "w") as file:
+                        file.write(json.dumps(results))
+                        print("File Opened")
+                        os.startfile("apiResults1.txt")
 
-            # if answerPrint == "u":
-
-                # updateURL = "https://api.reptimeqa.com/reptime/public/api/import/M32685/orders/a87e7307-ffc6-490a-b1ac-f1a5d8f6c719/status/update"
-                #
-                # payload = "Pending"
-                # headers = {
-                #     'x-api-key': 'd843ec7a50cf568b220e3ec6fb2bc795',
-                #     'Content-Type': 'text/plain',
-                # }
-                #
-                # response = requests.request("POST", updateURL, headers=headers, data=payload)
+                if answerMenu == "v":
+                    print("Here is the raw data:\n", response.content)
 
 
-    if answer == "n":
-        print("Thank you, see you later!")
+                if answerMenu == "q":
+                    print("\nThank you, see you soon!")
+                    break
 
-else:
-    print("There are no new orders")
+                # if answerPrint == "u":
+
+                    # updateURL = "https://api.reptimeqa.com/reptime/public/api/import/M32685/orders/a87e7307-ffc6-490a-b1ac-f1a5d8f6c719/status/update"
+                    #
+                    # payload = "Pending"
+                    # headers = {
+                    #     'x-api-key': 'd843ec7a50cf568b220e3ec6fb2bc795',
+                    #     'Content-Type': 'text/plain',
+                    # }
+                    #
+                    # response = requests.request("POST", updateURL, headers=headers, data=payload)
+
+
+        if answer == "n":
+            print("Thank you, see you later!")
+
+    else:
+        print("There are no new orders")
