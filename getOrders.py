5b3fefe6745506342e8325080af520e632ba2dae
Jeremy Burnham <25983307+JurassicJeremy@users.noreply.github.com>
2021-05-03 19:56:45 -0500

    Added option to write orders to file, and open if desired.

--- getOrders.py
+++ getOrders.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import os
 
 countURL = "https://api.reptimeqa.com/reptime/public/api/orders/export/M32685/count"
 
@@ -17,15 +18,29 @@ if ":1" in response.text:
 
     answer = input("Type y for yes, n for no: ", )
 
-    if answer != "n":
+    if answer == "y":
         detailsURL = "https://api.reptimeqa.com/reptime/public/api/orders/export/M32685/open"
 
         # GET API to obtain new order details
         response = requests.request("GET", detailsURL, headers=headers, data=payload)
-
         results = response.json()
 
-        print(results['response'][0]['pONumber'])
+        #Creates TXT file, and adds API response to file
+        with open("apiResults1.txt", "w") as file:
+            file.write(json.dumps(results))
+            print("Orders downloaded, would you like to open the resulting file?")
+            file.close()
+
+            answerPrint = input("Type y for yes, n for no: ", )
+
+            if answerPrint == "y":
+                #Opens the TXT file
+                os.startfile("apiResults1.txt")
+                print("Enjoy!")
+
+            if answerPrint == "n":
+                print("Thank you, see you soon!")
+
 
         # updateURL = "https://api.reptimeqa.com/reptime/public/api/import/M32685/orders/a87e7307-ffc6-490a-b1ac-f1a5d8f6c719/status/update"
         #
@@ -36,6 +51,9 @@ if ":1" in response.text:
         # }
         #
         # response = requests.request("POST", updateURL, headers=headers, data=payload)
+
+    if answer == "n":
+        print("Thank you, come again!")
 
 else:
     print("There are no new orders")
